536e6710c91828ffdf2fc91f555c83c77b8d49bc
dreik84 <dreik84@mail.ru>
2025-08-04 18:46:15 +0300

    Update Task.java

--- src/main/java/com/example/Task.java
+++ src/main/java/com/example/Task.java
@@ -1,8 +1,16 @@
 package com.example;
 
 public class Task {
+
     protected String name;
     protected String description;
     protected Long id;
-    protected String status;
+    protected Status status;
+
+    public Task(String name, String description, Status status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.id = TaskManager.generateId();
+    }
 }
