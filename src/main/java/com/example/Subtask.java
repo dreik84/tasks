f23cb43d217b426e6583b527b85561eeb1899def
dreik84 <dreik84@mail.ru>
2025-08-04 18:44:45 +0300

    Add Subtask.java

---
+++ src/main/java/com/example/Subtask.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Subtask extends Task {
+
+    public Subtask(String name, String description, Status status) {
+        super(name, description, status);
+    }
+}
