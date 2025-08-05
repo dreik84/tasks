0cfe1edfdf9d1733eb0ec8ec2906ec724a2eff48
dreik84 <dreik84@mail.ru>
2025-08-05 12:07:04 +0300

    Update TaskManager.java

--- src/main/java/com/example/TaskManager.java
+++ src/main/java/com/example/TaskManager.java
@@ -5,12 +5,45 @@ import java.util.Map;
 
 public class TaskManager {
 
-    private Map<Long, String> tasks = new HashMap<>();
-    private Map<Long, String> subTasks = new HashMap<>();
-    private Map<Long, String> epics = new HashMap<>();
     private static Long id = 0L;
+
+    private Map<Long, String> tasks;
+    private Map<Long, String> subTasks;
+    private Map<Long, String> epics;
+
+    public TaskManager() {
+        tasks = new HashMap<>();
+        subTasks = new HashMap<>();
+        epics = new HashMap<>();
+    }
 
     public static Long generateId() {
         return ++id;
+    }
+
+    public Map<Long, String> getTasks() {
+        return tasks;
+    }
+
+    public Map<Long, String> getSubTasks() {
+        return subTasks;
+    }
+
+    public Map<Long, String> getEpics() {
+        return epics;
+    }
+
+    public void removeTasks() {
+        for (var key : tasks.keySet()) {
+            tasks.remove(key);
+        }
+    }
+
+    public void removeSubTasks() {
+        subTasks = new HashMap<>();
+    }
+
+    public void removeEpics() {
+        epics = new HashMap<>();
     }
 }
