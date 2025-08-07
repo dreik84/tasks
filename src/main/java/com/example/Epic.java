fb2011d589fdcdf42a67cf6ecdea228796975fb8
dreik84 <dreik84@mail.ru>
2025-08-07 11:55:36 +0300

    Update Epic.java

--- src/main/java/com/example/Epic.java
+++ src/main/java/com/example/Epic.java
@@ -1,8 +1,22 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Epic extends Task {
+
+    private Map<Long, Subtask> subtasks;
 
     public Epic(String name, String description, Status status) {
         super(name, description, status);
+        subtasks = new HashMap<>();
+    }
+
+    public void addSubtask(Subtask subtask) {
+        subtasks.put(subtask.id, subtask);
+    }
+
+    public Map<Long, Subtask> getSubtasks() {
+        return subtasks;
     }
 }
