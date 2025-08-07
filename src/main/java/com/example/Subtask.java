85276b87edaa8c1fb910208abb0d8a1d4f852818
dreik84 <dreik84@mail.ru>
2025-08-07 11:55:52 +0300

    Update Subtask.java

--- src/main/java/com/example/Subtask.java
+++ src/main/java/com/example/Subtask.java
@@ -2,7 +2,10 @@ package com.example;
 
 public class Subtask extends Task {
 
-    public Subtask(String name, String description, Status status) {
+    private Long epicId;
+
+    public Subtask(String name, String description, Status status, Long epicId) {
         super(name, description, status);
+        this.epicId = epicId;
     }
 }
