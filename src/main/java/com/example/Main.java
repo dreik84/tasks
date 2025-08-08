11ccac4a6ca0b48d326d1722feec9a766ab1b3ab
dreik84 <dreik84@mail.ru>
2025-08-08 12:01:15 +0300

    Add Main.java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Task task1 = new Task("Починить забор",
+                "Нужно отремонтировать забор с использование подручного инструмента",
+                Status.NEW);
+
+        Task taks2 = new Task("Вынести мусор",
+                "Небходимо выбрость мусор в специальный контейнер",
+                Status.NEW);
+
+        Epic epic1 = new Epic("Выучить английский",
+                "Нужно выучить английский язык",
+                Status.NEW);
+
+        Subtask subtask1 = new Subtask("Купить учебник",
+                "Необходимо приобрести учебник по английскому языку",
+                Status.NEW,
+                epic1);
+
+        Subtask subtask2 = new Subtask("Выучить граматику языка",
+                "Начать изучать граматику английского языка",
+                Status.NEW,
+                epic1);
+
+        Epic epic2 = new Epic("Построить дом",
+                "Нужно построить жилой дом",
+                Status.NEW);
+
+        Subtask subtask3 = new Subtask("Заказать стройматериаллы",
+                "Необходимо заказать инструменты и материаллы для строительства",
+                Status.NEW,
+                epic2);
+    }
+}
