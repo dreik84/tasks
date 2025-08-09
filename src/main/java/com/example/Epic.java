11282d658029059c69e7eded382500ef3a8d8de3
dreik84 <dreik84@mail.ru>
2025-08-09 16:21:52 +0300

    Update Epic.java

--- src/main/java/com/example/Epic.java
+++ src/main/java/com/example/Epic.java
@@ -39,4 +39,14 @@ public class Epic extends Task {
         }
 
     }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
 }
