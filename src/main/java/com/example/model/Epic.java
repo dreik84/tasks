9f04f05dfe61e9d5ed487bce263a85faae8c479b
dreik84 <dreik84@mail.ru>
2025-08-12 12:09:27 +0300

    Update Epic.java, remove subtasks

--- src/main/java/com/example/model/Epic.java
+++ src/main/java/com/example/model/Epic.java
@@ -1,50 +1,9 @@
 package com.example.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Epic extends Task {
-
-    private final Map<Long, Subtask> subtasks;
 
     public Epic(String name, String description, Status status) {
         super(name, description, status);
-        subtasks = new HashMap<>();
-    }
-
-    public void addSubtask(Subtask subtask) {
-        subtasks.put(subtask.id, subtask);
-    }
-
-    public void removeSubtaskById(Long id) {
-        subtasks.remove(id);
-    }
-
-    public Map<Long, Subtask> getSubtasks() {
-        return subtasks;
-    }
-
-    public void removeAllSubtasks() {
-        subtasks.clear();
-    }
-
-    public void updateStatus() {
-
-        boolean isAllNew = subtasks.values().stream()
-                .filter(subtask -> subtask.status == Status.NEW)
-                .count() == subtasks.size();
-
-        boolean isAllDone = subtasks.values().stream()
-                .filter(subtask -> subtask.status == Status.DONE)
-                .count() == subtasks.size();
-
-        if (subtasks.isEmpty() || isAllNew) {
-            status = Status.NEW;
-        } else if (isAllDone) {
-            status = Status.DONE;
-        } else {
-            status = Status.IN_PROCESS;
-        }
     }
 
     @Override
