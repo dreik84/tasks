c7c044bc45cccbe9196dc3e150c7538459638980
dreik84 <dreik84@mail.ru>
2025-08-13 12:35:24 +0300

    Update Epic.java, refactor toString method

--- src/main/java/com/example/model/Epic.java
+++ src/main/java/com/example/model/Epic.java
@@ -8,9 +8,9 @@ public class Epic extends Task {
 
     @Override
     public String toString() {
-        return "Epic{" + "name='" + name
-                + '\'' + ", description='" + description + '\''
-                + ", id=" + id
-                + ", status=" + status + '}';
+        return "Epic{" + "name='" + getName() + '\''
+                + ", description='" + getDescription() + '\''
+                + ", id=" + getId()
+                + ", status=" + getStatus() + '}';
     }
 }
