5dffed9aa5e78c5a87f8912dabe72daf280a0f40
dreik84 <dreik84@mail.ru>
2025-08-13 12:36:40 +0300

    Update Task.java, update priority policy

--- src/main/java/com/example/model/Task.java
+++ src/main/java/com/example/model/Task.java
@@ -4,10 +4,10 @@ import com.example.service.TaskManager;
 
 public class Task {
 
-    protected String name;
-    protected String description;
-    protected Long id;
-    protected Status status;
+    private String name;
+    private String description;
+    private final Long id;
+    private Status status;
 
     public Task(String name, String description, Status status) {
         this.name = name;
@@ -26,6 +26,22 @@ public class Task {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
