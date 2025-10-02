193ff073e8bf02175de934d0d010c5ff63c03519
dreik84 <dreik84@mail.ru>
2025-10-02 10:19:25 +0300

    Update FileBackendTasksManager.java, add toString

--- src/main/java/com/example/service/FileBackendTasksManager.java
+++ src/main/java/com/example/service/FileBackendTasksManager.java
@@ -3,10 +3,12 @@ package com.example.service;
 import com.example.model.Epic;
 import com.example.model.Subtask;
 import com.example.model.Task;
+import com.example.model.TaskType;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.StringJoiner;
 
 public class FileBackendTasksManager extends InMemoryTasksManager implements TaskManager {
 
@@ -41,5 +43,22 @@ public class FileBackendTasksManager extends InMemoryTasksManager implements Tas
     public void addEpic(Epic epic) throws IOException {
         epics.put(epic.getId(), epic);
         save();
+    }
+
+    public String toString(Task task) {
+        TaskType taskType;
+        String epic = "";
+
+        if (task instanceof Subtask) {
+            taskType = TaskType.SUBTASK;
+            epic = String.valueOf(((Subtask) task).getEpicId());
+        } else if (task instanceof Epic) {
+            taskType = TaskType.EPIC;
+        } else {
+            taskType = TaskType.TASK;
+        }
+
+        return task.getId() + "," + taskType + "," + task.getName() + ","
+                + task.getStatus() + "," + task.getDescription() + "," + epic;
     }
 }
