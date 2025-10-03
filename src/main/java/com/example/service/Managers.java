679cbc917aa1e7c48c347e6dabb08d6cd87f56cb
dreik84 <dreik84@mail.ru>
2025-10-03 10:12:05 +0300

    Update Managers.java

--- src/main/java/com/example/service/Managers.java
+++ src/main/java/com/example/service/Managers.java
@@ -1,9 +1,15 @@
 package com.example.service;
 
+import java.nio.file.Path;
+
 public class Managers {
 
     public static TaskManager getDefault() {
         return new InMemoryTasksManager();
+    }
+
+    public static FileBackendTasksManager getFileBackendManager(Path filePath) {
+        return new FileBackendTasksManager(filePath);
     }
 
     public static HistoryManager getDefaultHistory() {
