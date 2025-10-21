11b34da69e607cc8517ef9fe3fa1364cded88832
dreik84 <dreik84@mail.ru>
2025-10-21 10:26:53 +0300

    Update Epic.java

--- src/main/java/com/example/model/Epic.java
+++ src/main/java/com/example/model/Epic.java
@@ -1,13 +1,20 @@
 package com.example.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Epic extends Task {
+
+    private Set<Long> subtasks;
 
     public Epic(String name, String description, Status status) {
         super(name, description, status);
+        subtasks = new HashSet<>();
     }
 
     public Epic(Long id, String name, String description, Status status) {
         super(id, name, description, status);
+        subtasks = new HashSet<>();
     }
 
     @Override
@@ -16,5 +23,17 @@ public class Epic extends Task {
                 + ", description='" + getDescription() + '\''
                 + ", id=" + getId()
                 + ", status=" + getStatus() + '}';
+    }
+
+    public Set<Long> getSubtasks() {
+        return subtasks;
+    }
+
+    public void addSubtask(Long id) {
+        subtasks.add(id);
+    }
+
+    public void removeSubtask(Long id) {
+        subtasks.remove(id);
     }
 }
