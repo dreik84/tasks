e3dba624a5a4eed37001df5582cb63964c11f62a
dreik84 <dreik84@mail.ru>
2025-10-22 09:34:07 +0300

    Update EpicTest.java

--- src/test/java/com/example/model/EpicTest.java
+++ src/test/java/com/example/model/EpicTest.java
@@ -1,13 +1,19 @@
 package com.example.model;
 
+import com.example.service.InMemoryTasksManager;
+import com.example.service.TaskManager;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EpicTest {
 
     @Test
-    void testEpicStatus() {
+    void testEpicStatus() throws IOException {
+        TaskManager taskManager = new InMemoryTasksManager();
+
         Epic epic = new Epic("Выучить английский",
                 "Нужно выучить английский язык",
                 Status.NEW);
@@ -24,12 +30,19 @@ class EpicTest {
                 Status.NEW,
                 epic.getId());
 
+        taskManager.addEpic(epic);
+        taskManager.addSubtask(subtask1);
+        taskManager.addSubtask(subtask2);
+
         assertEquals(Status.NEW, epic.getStatus());
 
-        subtask1.setStatus(Status.DONE);
-        subtask2.setStatus(Status.DONE);
+        taskManager.updateStatus(subtask1, Status.IN_PROCESS);
+
+        assertEquals(Status.IN_PROCESS, epic.getStatus());
+
+        taskManager.updateStatus(subtask1, Status.DONE);
+        taskManager.updateStatus(subtask2, Status.DONE);
 
         assertEquals(Status.DONE, epic.getStatus());
     }
-
 }
