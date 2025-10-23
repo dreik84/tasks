0a54fa21f1c908f04b6451041829d9e3813884c6
dreik84 <dreik84@mail.ru>
2025-10-23 10:15:34 +0300

    Create TaskManagerTest.java

---
+++ src/test/java/com/example/service/TaskManagerTest.java
@@ -0,0 +1,88 @@
+package com.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskManagerTest {
+
+    @Test
+    void getHistory() {
+    }
+
+    @Test
+    void getTasks() {
+    }
+
+    @Test
+    void getSubtasks() {
+    }
+
+    @Test
+    void getEpics() {
+    }
+
+    @Test
+    void removeAllTasks() {
+    }
+
+    @Test
+    void removeAllSubtasks() {
+    }
+
+    @Test
+    void removeAllEpics() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void getSubtaskById() {
+    }
+
+    @Test
+    void getEpicById() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void addSubtask() {
+    }
+
+    @Test
+    void addEpic() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void updateSubtask() {
+    }
+
+    @Test
+    void updateEpic() {
+    }
+
+    @Test
+    void removeTaskById() {
+    }
+
+    @Test
+    void removeSubtaskById() {
+    }
+
+    @Test
+    void removeEpicById() {
+    }
+
+    @Test
+    void updateStatus() {
+    }
+}
